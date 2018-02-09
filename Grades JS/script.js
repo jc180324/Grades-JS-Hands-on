14b8cf9d6b99aa29d6b9c640505ed2b744f8bd36
jc180324 <31575647+jc180324@users.noreply.github.com>
2018-02-09 16:54:44 +0100

    Add files via upload

---
+++ Grades JS/script.js
@@ -0,0 +1,25 @@
+function caculatedGrade(grade) {
+
+}
+
+var inputGrade = prompt("Enter a grade");
+
+if (inputGrade >= 100){
+    prompt ("A+ congrats smarty pants");
+}
+if (inputGrade >= 90 && inputGrade <= 99) {
+  prompt("A you still are awesome");
+}
+if (inputGrade >= 80 && inputGrade <=89) {
+  prompt("B you be alright");
+}
+if (inputGrade >= 70 && inputGrade <=79) {
+  prompt("C I see you, keep going");
+}
+if (inputGrade >= 60 && inputGrade <=69) {
+  prompt("D Oh no, you didn't!");
+}
+if (inputGrade >= 50 && inputGrade <=59) {
+  prompt("F aint nonody got time for that!");
+}
+
